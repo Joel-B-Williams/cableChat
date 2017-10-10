c4f8e824e68017db1da2834fe65e8646f040e48d
Joel <joel.b.williams.is@gmail.com>
2017-10-09 20:32:50 -0500

    broadcast job & message rendering through controller

---
+++ app/jobs/message_broadcast_job.rb
@@ -0,0 +1,15 @@
+class MessageBroadcastJob < ApplicationJob 
+  queue_as :default
+
+  #this method does the actual broadcasting. 
+  def perform(message)
+    ActionCable.server.broadcast "chat_rooms_#{message.chat_room.id}_channel", message: "MESSAGE_HTML"
+  end
+
+  private
+
+    #renders _message partial with help from message controller (created just for this)
+    def render_message(message)
+      MessageController.render partial: 'mesages/message', locals: {message: message}
+    end
+end
--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,7 @@ class Message < ApplicationRecord
   belongs_to :chat_room
 
   validates :body, presence: true, length: {minimum: 1, maximum: 1000}
+  
+  #pass a saved message (self) to message broadcast job
+  after_create_commit { MessageBroadcastJob.perform_later(self) }
 end
