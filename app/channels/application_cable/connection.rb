a1c528aa22c55ae6d1ce44d94265a8929c545311
Joel <joel.b.williams.is@gmail.com>
2017-10-09 20:22:29 -0500

    add notes

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -4,13 +4,13 @@ module ApplicationCable
 
     def connect 
       self.current_user = find_verified_user
-      logger.add_tags 'Actioncable', current_user.email
+      logger.add_tags 'Actioncable', current_user.email #displays log/debug info in console
     end
 
     protected
 
     def find_verified_user #checks if user auth'd with devise
-      if verified_user = env['warden'].user
+      if verified_user = env['warden'].user #warden is used by devise, this essentially verifies the logged-in user in a similar way that a homebrew current_user would (ish)
         verified_user
       else
         reject_unauthorized_connection
