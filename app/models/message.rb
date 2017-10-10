a0456a2c78f8e36bc411a873cb2fcdad28488286
Joel <joel.b.williams.is@gmail.com>
2017-10-10 14:31:10 -0500

    move timestamp method to message

--- app/models/message.rb
+++ app/models/message.rb
@@ -6,4 +6,8 @@ class Message < ApplicationRecord
   
   #pass a saved message (self) to message broadcast job
   after_create_commit { MessageBroadcastJob.perform_later(self) }
+
+  def timestamp
+    created_at.strftime('%H:%M:%S %d %B %Y')
+  end
 end
