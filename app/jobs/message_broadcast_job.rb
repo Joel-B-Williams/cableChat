6571988ff312f5c89f1c297d2d2eaaad639b4d9a
Joel <joel.b.williams.is@gmail.com>
2017-10-09 22:39:16 -0500

    update to use render method

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -3,7 +3,7 @@ class MessageBroadcastJob < ApplicationJob
 
   #this method does the actual broadcasting. 
   def perform(message)
-    ActionCable.server.broadcast "chat_rooms_#{message.chat_room.id}_channel", message: "MESSAGE_HTML"
+    ActionCable.server.broadcast "chat_rooms_#{message.chat_room.id}_channel", message: render_message(message)
   end
 
   private
